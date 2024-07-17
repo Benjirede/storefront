de881824a3041c29268fdea467ef1bf356db2498
Benjirede <benjaminzpalmer@gmail.com>
2024-07-17 16:53:40 -0600

    Finished Django ORM section of course

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,15 @@
+from django.forms import DecimalField
 from django.shortcuts import render
-from django.db.models import Q, F
-from store.models import Product, OrderItem
+from django.db import transaction
+from django.db.models import Q, F, Value, Func, ExpressionWrapper
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
+from django.db.models.functions import Concat
+from django.contrib.contenttypes.models import ContentType
+from store.models import Collection, Product, OrderItem, Order, Customer
+from tags.models import TaggedItem
 
-# Create your views here.
+
 def say_hello(request):
-    queryset = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+    queryset = Product.objects.raw('select * from store_product')
     
-    return render(request, 'hello.html', {'name': 'Ben', 'products': list(queryset)})
+    return render(request, 'hello.html', {'name': 'Ben', 'result': list(queryset)})
