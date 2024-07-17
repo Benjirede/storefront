f1f55963f131875dff4b8b03a55eea1fc3c0bcf2
Benjirede <benjaminzpalmer@gmail.com>
2024-07-17 13:17:44 -0600

    first commit

---
+++ playground/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.db.models import Q, F
+from store.models import Product, OrderItem
+
+# Create your views here.
+def say_hello(request):
+    queryset = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+    
+    return render(request, 'hello.html', {'name': 'Ben', 'products': list(queryset)})
