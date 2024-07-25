c7b74ce32e932fc973a5f17222731fc8db04178d
Benjirede <benjaminzpalmer@gmail.com>
2024-07-25 13:38:18 -0600

    changes made for part 2 of django course

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,5 @@
-from django.forms import DecimalField
 from django.shortcuts import render
-from django.db import transaction
-from django.db.models import Q, F, Value, Func, ExpressionWrapper
-from django.db.models.aggregates import Count, Max, Min, Avg, Sum
-from django.db.models.functions import Concat
-from django.contrib.contenttypes.models import ContentType
-from store.models import Collection, Product, OrderItem, Order, Customer
-from tags.models import TaggedItem
 
 
 def say_hello(request):
-    queryset = Product.objects.raw('select * from store_product')
-    
-    return render(request, 'hello.html', {'name': 'Ben', 'result': list(queryset)})
+    return render(request, 'hello.html', {'name': 'Ben'})
--- storefront/urls.py
+++ storefront/urls.py
@@ -1,5 +1,4 @@
-"""
-URL configuration for storefront project.
+"""storefront URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,9 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
+import debug_toolbar
 
 admin.site.site_header = 'Storefront Admin'
 admin.site.index_title = 'Admin'
@@ -24,5 +23,5 @@ admin.site.index_title = 'Admin'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls')),
-    path('_debug_/', include(debug_toolbar.urls))
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
